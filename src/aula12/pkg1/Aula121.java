0c27b6eb7bfc6b3e7895cb89053a275fd9bfec08
11944413600 <11944413600@lab06m43.nrc.ice.ufjf.br>
2017-10-10 22:20:14 -0300

    Primeiro commit

---
+++ src/aula12/pkg1/Aula121.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula12.pkg1;
+
+/**
+ *
+ * @author 11944413600
+ */
+public class Aula121 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JanelaArquivo janela = new JanelaArquivo();
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+    }
+    
+}
